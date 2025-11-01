18045fe07322215ac0d110a6607175e13d691f38
sathishd_recode <sathishkumar.durairaj@kamerai.ai>
2025-11-01 09:52:57 +0530

    Initial code commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <p className="bg-blue-200 text-center p-4 rounded-lg">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
